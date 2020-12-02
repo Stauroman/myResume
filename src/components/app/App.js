821a48d230be306d6f28964b98065c11edde6ac6
Stauroman <62332038+Stauroman@users.noreply.github.com>
2020-12-02 10:38:18 +0700

    Initial commit

---
+++ src/components/app/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header >
+
+        <a>
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
