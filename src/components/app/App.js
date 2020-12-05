718c8e625278c68a318e01970b232d4849298895
Stauroman <62332038+Stauroman@users.noreply.github.com>
2020-12-05 16:31:01 +0700

    решаются задачи по реакту

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,15 +1,42 @@
+import React, {Component} from "react"
+
 import './App.css';
 
-function App() {
-  return (
-    <div >
-      <header >
+class App extends Component {
 
-        <span>
-          Learn Reactацуацуаыу</span>
-      </header>
-    </div>
-  );
+    state= {
+        hello: true,
+        name: "Ivan",
+        age: 25,
+        isVisible: false
+    };
+
+    showName = () =>{
+        alert("Hello " + this.state.name)
+    }
+    changeUser = () => {
+        this.setState({name: 'Nik', age: 30});
+    }
+
+    helloOrBye = () => {this.setState({hello: !this.state.hello})}
+    showContent = () => {this.setState({isVisible : !this.state.isVisible})}
+
+
+
+    render() {
+    const {name, age, hello} = this.state;
+
+        return (
+            <div>
+                <p>{hello ? "Hello" : "Bye"} {name}, возраст: {age}</p>
+                <br />
+                <button onClick={this.showName}>Alert Button</button>
+                <button onClick={this.changeUser}>ChangeUser</button>
+                <button onClick={this.showContent}>ShowContent</button>
+                <button onClick={this.helloOrBye}>{hello ? "exit" : "enter"}</button>
+            </div>
+
+        )
+    }
 }
-
 export default App;
