96a25dbb945005853f8187cb8ce71327c04f44e4
Stauroman <62332038+Stauroman@users.noreply.github.com>
2020-12-05 18:45:41 +0700

    решены задачи 4х уроков

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,39 +4,71 @@ import './App.css';
 
 class App extends Component {
 
-    state= {
+    state = {
         hello: true,
         name: "Ivan",
         age: 25,
-        isVisible: false
+        isVisible: false,
+        users: ["Ivan", "Boris", "Roman", "Alex"],
+        hrefs: [
+            {href: '1.html', text: 'ссылка 1'},
+            {href: '2.html', text: 'ссылка 2'},
+            {href: '3.html', text: 'ссылка 3'},
+        ]
     };
 
-    showName = () =>{
+    showName = () => {
         alert("Hello " + this.state.name)
     }
     changeUser = () => {
         this.setState({name: 'Nik', age: 30});
     }
 
-    helloOrBye = () => {this.setState({hello: !this.state.hello})}
-    showContent = () => {this.setState({isVisible : !this.state.isVisible})}
-
+    helloOrBye = () => {
+        this.setState({hello: !this.state.hello})
+    }
+    toggleVisible = () => {
+        this.setState({isVisible: !this.state.isVisible})
+    }
 
 
     render() {
-    const {name, age, hello} = this.state;
 
+        const {name, age, hello, isVisible} = this.state;
+        const content = <p>{hello ? "Hello" : "Bye"} {name}, возраст: {age}</p>;
+        const visibleContent = isVisible ? content : null;
+
+        const usersList = this.state.users.map((item, index) => {
+                return (
+                    <li key={index}>{item} - {index + 1}</li>
+                )
+            }
+        )
+        const hrefList = this.state.hrefs.map((item, index) => {
+            return (
+                <li key={index}><a href={item.href}>{item.text}</a></li>
+            )
+        })
         return (
             <div>
-                <p>{hello ? "Hello" : "Bye"} {name}, возраст: {age}</p>
-                <br />
+                {visibleContent}
+                <ul>
+                    {hrefList}
+                </ul>
+                <br/>
+                <ul>
+                    {usersList}
+                </ul>
+
+                <br/>
                 <button onClick={this.showName}>Alert Button</button>
                 <button onClick={this.changeUser}>ChangeUser</button>
-                <button onClick={this.showContent}>ShowContent</button>
+                <button onClick={this.toggleVisible}>{isVisible ? "HideContent" : "ShowContent"}</button>
                 <button onClick={this.helloOrBye}>{hello ? "exit" : "enter"}</button>
             </div>
 
         )
     }
 }
+
 export default App;
