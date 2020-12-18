4ff03530546bb7cec6703c95cf523a78511490aa
Stauroman <62332038+Stauroman@users.noreply.github.com>
2020-12-18 13:14:17 +0700

    форма регистрации с валидацией и записью данных в localStorage

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -8,16 +8,19 @@ export default class RegistrationForm extends Component {
         name: "",
         surname: "",
         password: "",
+        rePassword: "",
         isInfoVisible: false,
         isEmailValid: false,
         isPasswordValid: false,
+        isRePasswordValid: false,
         isFormValid: false,
-        formErrors: {email: "", password: ""}
+        formErrors: {email: "", password: "", rePassword: ""},
     }
 
     validation = (fieldName, value) => {
         let emailValid = this.state.isEmailValid;
         let passwordValid = this.state.isPasswordValid;
+        let rePasswordValid = this.state.isRePasswordValid;
         let formErrors = this.state.formErrors;
         switch (fieldName) {
             case "email":
@@ -25,8 +28,12 @@ export default class RegistrationForm extends Component {
                 formErrors.email = emailValid ? "" : "Введите корректный адрес электронной почты";
                 break;
             case "password":
-                passwordValid = value.length >= 8;
+                passwordValid = value.length >= 2;
                 formErrors.password = passwordValid ? "" : "Пароль слишком короткий";
+                break;
+            case "rePassword":
+                rePasswordValid = this.state.rePassword === this.state.password;
+                formErrors.rePassword = rePasswordValid ? "" : "Пароли не совпадают";
                 break;
             default:
                 break;
@@ -37,7 +44,7 @@ export default class RegistrationForm extends Component {
             isPasswordValid: Boolean(passwordValid),
             formErrors: formErrors,
             isFormValid: emailValid &&
-                passwordValid
+                passwordValid && rePasswordValid
         });
     }
 
@@ -50,11 +57,18 @@ export default class RegistrationForm extends Component {
                 this.validation(name, value)
             })
     }
-    logger = () => {
-        console.log(this.state.isEmailValid);
-        console.log(this.state.isPasswordValid);
-        console.log(this.state.formErrors);
-        console.log(this.state.isFormValid);
+    //save user data in local storage, clear state.
+    saveUserInfo = () => {
+        const userInfo = {
+            email: this.state.email,
+            login: this.state.login,
+            name: this.state.name,
+            surname: this.state.surname,
+            password: this.state.password
+        }
+        const strUserInfo = JSON.stringify(userInfo);
+        localStorage.setItem(`${userInfo.login}`, strUserInfo);
+
     }
 
     render() {
@@ -75,6 +89,20 @@ export default class RegistrationForm extends Component {
                            value={this.state.login}/>
                 </div>
                 <div className="form-group">
+                    <label htmlFor="name">Введите ваше имя</label>
+                    <input name="name" type="text" className="form-control" id="name"
+                           placeholder="Введите имя"
+                           onChange={this.handleChange}
+                           value={this.state.name}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="surname">Введите вашу фамилию</label>
+                    <input name="surname" type="text" className="form-control" id="surname"
+                           placeholder="Введите фамилию"
+                           onChange={this.handleChange}
+                           value={this.state.surname}/>
+                </div>
+                <div className="form-group">
                     <label htmlFor="password">придумайте пароль, не менее 8ми символов</label>
                     <input name="password" type="password" className="form-control" id="password"
                            placeholder="Введите пароль"
@@ -82,15 +110,16 @@ export default class RegistrationForm extends Component {
                            value={this.state.password}/>
                 </div>
                 <div className="form-group">
-                    <label htmlFor="password">повторите пароль</label>
-                    <input name="password" type="password" className="form-control" id="password"
+                    <label htmlFor="rePassword">повторите пароль</label>
+                    <input name="rePassword" type="password" className="form-control" id="rePassword"
                            placeholder="Повторите пароль"
                            onChange={this.handleChange}
-                           value={this.state.password}/>
+                           value={this.state.rePassword}/>
                 </div>
-                    {/*<button onSubmit={}>Зарегестрироваться</button>*/}
-                    <button type="submit" className="btn btn-primary"
-                        onClick={this.logger}>valid?</button>
+                <button className="btn btn-primary" disabled={!this.state.isFormValid}
+                        onClick={this.saveUserInfo}>Зарегистрироваться
+                </button>
+
             </form>
         )
     }
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,130 +1,129 @@
 import React, {Component} from "react"
 
 import './App.css';
-import ChangeUser from '../ChangeUser'
 import RegistrationForm from "../RegistrationForm";
 
 class App extends Component {
-
-    state = {
-        hello: true,
-        isVisible: true,
-        name: "User",
-        age: "",
-        arrNum: 0,
-        users: ["Ivan", "Boris", "Roman", "Alex"],
-        hrefs: [
-            {href: '1.html', text: 'ссылка 1'},
-            {href: '2.html', text: 'ссылка 2'},
-            {href: '3.html', text: 'ссылка 3'},
-        ],
-        fullNameStr: "",
-        value: ""
-    };
-
-    greetUser = () => {
-        alert("Hello " + this.state.name)
-    }
-
-    handleChangeUser = () => {
-        const {arrNum, users} = this.state;
-        let returnValue = users[arrNum];
-        this.setState({arrNum: arrNum + 1});
-        if (arrNum === users.length - 1) {
-            this.setState({arrNum: 0});
-        }
-        this.setState({name: returnValue})
-    }
-    helloOrBye = () => {
-        this.setState({hello: !this.state.hello})
-    }
-    toggleVisibleInfo = () => {
-        this.setState({isVisible: !this.state.isVisible})
-    }
-
-    addUserToList = () => {
-        this.state.users.push("пункт");
-        this.setState({users: this.state.users})
-    };
-
-    deleteUserFromList = (index) => {
-        this.state.users.splice(index, 1)
-        this.setState({users: this.state.users})
-    };
-
-    handleChange = (event) => {
-        this.setState({[event.target.name]: event.target.value})
-    }
-    //вычисление года рождения:
-    getBirthYear = () => {
-        return new Date().getFullYear() - this.state.age
-
-    }
+    //
+    // state = {
+    //     hello: true,
+    //     isVisible: true,
+    //     name: "User",
+    //     age: "",
+    //     arrNum: 0,
+    //     users: ["Ivan", "Boris", "Roman", "Alex"],
+    //     hrefs: [
+    //         {href: '1.html', text: 'ссылка 1'},
+    //         {href: '2.html', text: 'ссылка 2'},
+    //         {href: '3.html', text: 'ссылка 3'},
+    //     ],
+    //     fullNameStr: "",
+    //     value: ""
+    // };
+    //
+    // greetUser = () => {
+    //     alert("Hello " + this.state.name)
+    // }
+    //
+    // handleChangeUser = () => {
+    //     const {arrNum, users} = this.state;
+    //     let returnValue = users[arrNum];
+    //     this.setState({arrNum: arrNum + 1});
+    //     if (arrNum === users.length - 1) {
+    //         this.setState({arrNum: 0});
+    //     }
+    //     this.setState({name: returnValue})
+    // }
+    // helloOrBye = () => {
+    //     this.setState({hello: !this.state.hello})
+    // }
+    // toggleVisibleInfo = () => {
+    //     this.setState({isVisible: !this.state.isVisible})
+    // }
+    //
+    // addUserToList = () => {
+    //     this.state.users.push("пункт");
+    //     this.setState({users: this.state.users})
+    // };
+    //
+    // deleteUserFromList = (index) => {
+    //     this.state.users.splice(index, 1)
+    //     this.setState({users: this.state.users})
+    // };
+    //
+    // handleChange = (event) => {
+    //     this.setState({[event.target.name]: event.target.value})
+    // }
+    // //вычисление года рождения:
+    // getBirthYear = () => {
+    //     return new Date().getFullYear() - this.state.age
+    //
+    // }
 
     render() {
 
-        const {name, age, hello, isVisible} = this.state;
-        const content = <p>{hello ? "Hello" : "Bye"} {name}, возраст: {age}</p>;
-        const visibleContent = isVisible ? content : null;
-
-        const usersList = this.state.users.map((item, index) => {
-                return (
-                    <li key={index}>{item} - {index + 1}
-                        <button onClick={this.deleteUserFromList.bind(this, index)}>delete</button>
-                    </li>
-                )
-            }
-        )
-
-        const hrefList = this.state.hrefs.map((item, index) => {
-            return (
-                <li key={index}><a href={item.href}>{item.text}</a></li>
-            )
-        })
-
-        const fullName = this.state.fullNameStr.split(" ");
-        const lastName = fullName[0];
-        const firstName = fullName[1];
-        const patronymic = fullName[2];
+        // const {name, age, hello, isVisible} = this.state;
+        // const content = <p>{hello ? "Hello" : "Bye"} {name}, возраст: {age}</p>;
+        // const visibleContent = isVisible ? content : null;
+        //
+        // const usersList = this.state.users.map((item, index) => {
+        //         return (
+        //             <li key={index}>{item} - {index + 1}
+        //                 <button onClick={this.deleteUserFromList.bind(this, index)}>delete</button>
+        //             </li>
+        //         )
+        //     }
+        // )
+        //
+        // const hrefList = this.state.hrefs.map((item, index) => {
+        //     return (
+        //         <li key={index}><a href={item.href}>{item.text}</a></li>
+        //     )
+        // })
+        //
+        // const fullName = this.state.fullNameStr.split(" ");
+        // const lastName = fullName[0];
+        // const firstName = fullName[1];
+        // const patronymic = fullName[2];
         return (
             <div>
                 <header>
-                    <div>Добро пожаловать в мой учебный проект </div>
+                    <div>Добро пожаловать в мой учебный проект, зарегистрируйтесь для продолжения работы</div>
                 </header>
-                {visibleContent}
-                <ul>
-                    {hrefList}
-                </ul>
-                <br/>
-                <ul>
-                    {usersList}
-                </ul>
+                {/*{visibleContent}*/}
+                {/*<ul>*/}
+                {/*    {hrefList}*/}
+                {/*</ul>*/}
+                {/*<br/>*/}
+                {/*<ul>*/}
+                {/*    {usersList}*/}
+                {/*</ul>*/}
 
-                <input name="age"
-                       type="number"
-                       placeholder={"введите ваш возраст"}
-                       onChange={this.handleChange}
-                       value={this.state.age}
-                />
-                <br/>
-                <span>год рождения {this.getBirthYear()}</span>
-                <br/>
-                <input type="text"
-                       placeholder="Введите Фамилию Имя Отчество через пробел"
-                       name="fullNameStr"
-                       onChange={this.handleChange}
-                       value={this.state.fullNameStr}
-                />
-                <br/>
-                <p>{lastName}</p>
-                <p>{firstName}</p>
-                <p>{patronymic}</p>
-                <button onClick={this.greetUser}>Greet User</button>
-                <button onClick={this.addUserToList}>add element</button>
-                <button onClick={this.deleteUserFromList.bind(this, 0)}>Delete element</button>
-                <button onClick={this.toggleVisibleInfo}>{isVisible ? "Hide User Info" : "Show User Info"}</button>
-                <ChangeUser onClick={() => this.handleChangeUser()}/>
-                <button onClick={this.helloOrBye}>{hello ? "exit" : "enter"}</button>
+                {/*<input name="age"*/}
+                {/*       type="number"*/}
+                {/*       placeholder={"введите ваш возраст"}*/}
+                {/*       onChange={this.handleChange}*/}
+                {/*       value={this.state.age}*/}
+                {/*/>*/}
+                {/*<br/>*/}
+                {/*<span>год рождения {this.getBirthYear()}</span>*/}
+                {/*<br/>*/}
+                {/*<input type="text"*/}
+                {/*       placeholder="Введите Фамилию Имя Отчество через пробел"*/}
+                {/*       name="fullNameStr"*/}
+                {/*       onChange={this.handleChange}*/}
+                {/*       value={this.state.fullNameStr}*/}
+                {/*/>*/}
+                {/*<br/>*/}
+                {/*<p>{lastName}</p>*/}
+                {/*<p>{firstName}</p>*/}
+                {/*<p>{patronymic}</p>*/}
+                {/*<button onClick={this.greetUser}>Greet User</button>*/}
+                {/*<button onClick={this.addUserToList}>add element</button>*/}
+                {/*<button onClick={this.deleteUserFromList.bind(this, 0)}>Delete element</button>*/}
+                {/*<button onClick={this.toggleVisibleInfo}>{isVisible ? "Hide User Info" : "Show User Info"}</button>*/}
+                {/*<ChangeUser onClick={() => this.handleChangeUser()}/>*/}
+                {/*<button onClick={this.helloOrBye}>{hello ? "exit" : "enter"}</button>*/}
                 <RegistrationForm />
             </div>
 
