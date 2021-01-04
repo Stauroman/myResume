e503ed278ec77aeced24570e75c00758094459e3
Stauroman <62332038+Stauroman@users.noreply.github.com>
2021-01-04 17:40:32 +0700

    header styles

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,21 +2,30 @@ import React from 'react'
 import {Link} from "react-router-dom";
 import './header.css'
 
+
+
 const Header =()=>{
     return (
-        <div className="header d-flex">
-            <h3 >
-                <a>Тут будет название проекта</a>
-            </h3>
-            <ul >
-                <li>
-                    <Link to="/registration">Зарегистрироваться</Link>
-                </li>
-                <li>
-                    <Link to="/login">Войти</Link>
-                </li>
+        <div >
 
+            <ul className="header nav">
+                <li className="nav-item">
+                    <Link to="/"
+                          className="nav-link link ">Тут будет название проекта</Link>
+                </li>
+                <li className="nav-item" style={{marginLeft: "auto"}}>
+                    <Link to="/registration"
+                          className="nav-link link"
+
+                    >Зарегистрироваться
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/login"
+                          className="nav-link link ">Войти</Link>
+                </li>
             </ul>
+
         </div>
     )
 }
